564d086f2a27e9e0c8884277ffb952396dd12c97
Hoang Phong <hoangvanphong579@gmail.com>
2022-12-12 21:13:45 +0700

    second commit

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-//filter creates a new array fill with elements that fulfill the condition provide by the filter
-let a=[1,2,3,4,5,6,7,8,9];
-a.forEach((value)=>{
-    console.log(value+1);
-})
 
+let a=[1,2,3,4,5,6,7,8,9];
+a.filter((a)=>a>2)
